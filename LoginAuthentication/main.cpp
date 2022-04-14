9ae54d1ac6b9680dbf654502b754107d80860e8e
Kdodeja2503 <96384940+Kdodeja2503@users.noreply.github.com>
2022-04-14 12:56:17 +0530

    Initial commit

---
+++ LoginAuthentication/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+bool IsLoggedIn() {
+	string username,password, un, pw;
+
+	cout << "Enter Username : ";
+	cin >> username;
+	cout << "Enter password : ";
+	cin >> password;
+
+	ifstream read("sample.txt");
+	getline(read, un);
+	getline(read, pw);
+
+	if (un == username && pw == password) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+
+int main() {
+	int choice;
+	cout << "1: Register\n2: Login\n3: Your Choice";
+	cin >> choice;
+	if (choice == 1) {
+		string username, password;
+		cout << "select Username";
+		cin >> username;
+		cout << "Select Paswword";
+		cin >> password;
+
+		ofstream file;
+		file.open("sample.txt");
+		file << username << endl << password;
+		file.close();
+
+		main();
+	}
+	else if (choice == 2) {
+		bool status = IsLoggedIn();
+
+		if (!status) {
+			cout << "Login Failed";
+			system("PAUSE");
+			return 0;
+		}
+		else {
+			cout << "Succesfully Logged In" << endl;
+			system("PAUSE");
+			return 1;
+		}
+
+	}
+
+}
+
